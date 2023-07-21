b6a6aee260180b14e13c8287ea44902f8d0e0580
elayomohammed <elayo.mohammed@gmail.com>
2023-07-21 07:47:33 +0100

    better updates

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -19,9 +19,46 @@ document.getElementById('open-menu').addEventListener('click', () =>{
     document.getElementById('nav-container').style.display = 'flex';
     document.getElementById('open-menu').style.display = 'none';
     document.getElementById('close-menu').style.display = 'block';
+    document.getElementById('welcomeMsg-top-container').style.filter = 'blur(1.5px)';
+    document.getElementById('aboutVisionMission-top-container').style.filter = 'blur(1.5px)';
+    document.getElementById('top-service-container').style.filter = 'blur(1.5px)';
+    document.getElementById('top-experience-container').style.filter = 'blur(1.5px)';
+    document.getElementById('top-team-container').style.filter = 'blur(1.5px)';
+    document.getElementById('top-contact-container').style.filter = 'blur(1.5px)';
+    document.getElementById('footer-section').style.filter = 'blur(1.5px)';
 });
 document.getElementById('close-menu').addEventListener('click', () =>{
     document.getElementById('nav-container').style.display = 'none';
     document.getElementById('close-menu').style.display = 'none';
     document.getElementById('open-menu').style.display = 'inline';
+    document.getElementById('welcomeMsg-top-container').style.filter = 'none';
+    document.getElementById('aboutVisionMission-top-container').style.filter = 'none';
+    document.getElementById('top-service-container').style.filter = 'none';
+    document.getElementById('top-experience-container').style.filter = 'none';
+    document.getElementById('top-team-container').style.filter = 'none';
+    document.getElementById('top-contact-container').style.filter = 'none';
+    document.getElementById('footer-section').style.filter = 'none';
+});
+
+// handiling viewing and closing welcome note
+document.getElementById('view-welcome-msg').addEventListener('click', () =>{
+    document.getElementById('welcomeMsg-container').style.display = 'block';
+    //document.getElementById('welcomeMsg-container').focus();
+    document.getElementById('header').style.filter = 'blur(1.5px)';
+    document.getElementById('aboutVisionMission-top-container').style.filter = 'blur(1.5px)';
+    document.getElementById('top-service-container').style.filter = 'blur(1.5px)';
+    document.getElementById('top-experience-container').style.filter = 'blur(1.5px)';
+    document.getElementById('top-team-container').style.filter = 'blur(1.5px)';
+    document.getElementById('top-contact-container').style.filter = 'blur(1.5px)';
+    document.getElementById('footer-section').style.filter = 'blur(1.5px)';
+});
+document.getElementById('msg-close-menu').addEventListener('click', () =>{
+    document.getElementById('welcomeMsg-container').style.display = 'none';
+    document.getElementById('header').style.filter = 'none';
+    document.getElementById('aboutVisionMission-top-container').style.filter = 'none';
+    document.getElementById('top-service-container').style.filter = 'none';
+    document.getElementById('top-experience-container').style.filter = 'none';
+    document.getElementById('top-team-container').style.filter = 'none';
+    document.getElementById('top-contact-container').style.filter = 'none';
+    document.getElementById('footer-section').style.filter = 'none';
 });
